5a7cae07daac6615b12505d60a43ed20d062bca6
Marco Cobian <maca9608@gmail.com>
2025-01-17 09:52:08 -0600

    Add other module relay on code

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Definir el pin GPIO al que está conectado el relay
-relay_pin = 21
+relay_pin = 21 # or pin 40
+relay_pin_two = 20 # or pin 38
 
 # Configurar el pin como salida
 GPIO.setup(relay_pin, GPIO.OUT)
+GPIO.setup(relay_pin_two, GPIO.OUT)
 
 try:
     # Encender el relay
     GPIO.output(relay_pin, GPIO.LOW)
+    GPIO.output(relay_pin_two, GPIO.LOW)
     print("Relay encendido")
-    time.sleep(5)  # Mantener el relay encendido por 5 segundos
+    time.sleep(10)  # Mantener el relay encendido por 5 segundos
 
     # Apagar el relay
     GPIO.output(relay_pin, GPIO.HIGH)
+    GPIO.output(relay_pin_two, GPIO.HIGH)
     print("Relay apagado")
 finally:
     # Limpiar la configuración de los pines GPIO
--- readme.md
+++ readme.md
@@ -11,4 +11,3 @@ Python 3.5.3
 ![alt text](image.png)
 
 ![alt text](image-1.png)
-
